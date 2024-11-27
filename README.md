14e804f34ccefed9a6d80ea97667ecb57a5d53d3
rodrigohurtadop <i202405517@cibertec.edu.pe>
2024-11-26 20:23:55 -0500

    challenge

---
+++ README.md
@@ -0,0 +1,8 @@
+<div align="center">
+  <h1 align="center">
+    Proyecto finalizado
+    <br />
+    <br />
+  </h1>
+</div>
+
---
+++ src/test/java/com/literatura/challenge_2_back_literatura/ChallengeLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literatura.challenge_2_back_literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
